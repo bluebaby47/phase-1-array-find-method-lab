8516b5b41b5e540d8e8816a43cfb7c063efa281f
kevin ikahu <kevinikahu4@gmail.com>
2023-03-30 15:11:52 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(record) {
+    const winRecord = record.find(season => season.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+  
